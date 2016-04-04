65882f38051a2ef6bd051d0112cf4a49940b80dc
Josh Hall <joshhall9@hotmail.com>
2016-04-04 10:18:11 -0700

    Initial Commit

---
+++ info498Dhw2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  info498Dhw2
+//
+//  Created by Joshua Hall on 4/4/16.
+//  Copyright © 2016 Joshua Hall. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
